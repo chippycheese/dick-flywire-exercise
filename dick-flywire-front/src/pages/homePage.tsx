96c343e2bfe189ac57899934609c511b5c7f4329
ChippyCheese <dillonjdick@gmail.com>
2024-12-11 20:05:47 -0600

    Select Employee Details

--- dick-flywire-front/src/pages/homePage.tsx
+++ dick-flywire-front/src/pages/homePage.tsx
@@ -10,6 +10,19 @@ function Home() {
 
   const [sortOrder, setSortOrder] = useState<'asc' | 'desc' | null>(null);
   const [sortColumn, setSortColumn] = useState<keyof Employee | null>(null);
+
+  const [selectedEmployee, setSelectedEmployee] = useState<Employee>(null);
+  const [directHires, setDirectHires] = useState<Employee[]>([]);
+
+  const handleSelect = (employee: Employee) => {
+    setSelectedEmployee(employee)
+    var dHires = []
+    employee.directReports.forEach(dHireId => {
+      dHires.push(employees.find(employee => employee.id == dHireId));
+    });
+    setDirectHires(dHires);
+  }
+
   const handleSort = (column: keyof Employee) => {
     if (sortColumn === column) {
       setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc');
@@ -42,25 +55,24 @@ function Home() {
       if (!response.ok) {
         throw new Error('Failed to fetch employees');
       }
-
       const eData: Employee[] = await response.json();
-      console.log(eData);
       setEmployees(eData);
+      setSelectedEmployee(null)
     } catch (error) {
       console.error('Error fetching employees:', error);
     } finally {
       setLoading(false);
     }
   };
-  
+
   return (
     <>
       <nav className="navbar navbar-dark bg-dark">
         <span className='title-text white title-padding'>Employee List</span>
       </nav>
-          {loading && <p>Loading...</p>}
-          {!loading && 
-          <table className="table table-striped">
+      {loading && <p>Loading...</p>}
+      {!loading &&
+        <table className={selectedEmployee ? 'table table-striped table-compress' : 'table table-striped'}>
           <thead>
             <tr>
               <th onClick={() => handleSort('id')} scope="col">Id {SortButton("id", sortColumn, sortOrder)}</th>
@@ -71,20 +83,39 @@ function Home() {
             </tr>
           </thead>
           <tbody>
-            
+
             {employees.map((employee: Employee) => (
               <tr key={employee.id}>
                 <th scope="row">{employee.id}</th>
-                <td>{employee.name}</td>
+                <td  >{employee.name}</td>
                 <td>{employee.position}</td>
                 <td>{employee.hireDate}</td>
-                <td>Details</td>
+                <td className='table-button btn btn-secondary' onClick={() => handleSelect(employee)} >Details</td>
               </tr>
-          
-          ))}
+
+            ))}
           </tbody>
-          </table>
+        </table>
+      }
+      {!loading && selectedEmployee && <>
+        <div className='v-bar' />
+        <div className='side-bar'>
+          <p><strong>Name :</strong>{" "+ selectedEmployee.name}</p>
+          <p><strong>Position :</strong>{" "+selectedEmployee.position}</p>
+          <p><strong>Hire Date :</strong>{" "+selectedEmployee.hireDate}</p>
+          {directHires != null && directHires.length > 0 && <>
+            <p><strong>Direct Reports :</strong></p>
+            {directHires.map((employee: Employee) => (
+              <ul>
+                <li key={employee.id}>{employee.name}</li>
+              </ul>
+            ))}
+          </>
           }
+
+
+        </div>
+      </>}
     </>
   );
 }
